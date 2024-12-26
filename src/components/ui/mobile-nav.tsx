541b16198c114f72c390ced1048f9a24aee7307e
Sasho Stojchevski <sasho.stojcevski@gmail.com>
2024-12-26 17:05:18 +0100

    feat: reserve btn float

--- src/components/ui/mobile-nav.tsx
+++ src/components/ui/mobile-nav.tsx
@@ -1,10 +1,15 @@
 'use client';
 
 import Link from 'next/link';
+import { addScrollingClass } from '@/utils/add-scrolling-class';
+import { useRef } from 'react';
 
 export default function MobileNav() {
+  const reserveRef = useRef(null);
+  addScrollingClass(reserveRef, 500);
+
   return (
-    <div className="sticky inset-x-0 bottom-0 z-50 grid h-16 w-full grid-cols-1 items-center justify-center gap-2 bg-black shadow-menu-shadow md:hidden">
+    <div ref={reserveRef} className="reserve-action-floating hidden sticky inset-x-0 bottom-0 z-50 h-16 w-full grid-cols-1 items-center justify-center gap-2 bg-black shadow-menu-shadow md:hidden">
       <div className="flex items-center justify-center">
         <Link href='#book' className="w-full h-full text-center text-white text-xl inline-block">
           Book Now
