369ab3972a5aa4752b6c6ffd13fefc2fed8880d8
Sasho Stojchevski <sasho.stojcevski@gmail.com>
2026-03-12 15:34:23 +0100

    clerk public key

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-   
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: 'pk_live_Y2xlcmsuemFpYS1zdWl0ZXMuY29tJA',
   },
   reactStrictMode: true,
   images: {
