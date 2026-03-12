22a2f5484383d7b96762f24a46db7b4137defd3e
Sasho Stojchevski <sasho.stojcevski@gmail.com>
2026-03-12 15:42:39 +0100

    node version

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   env: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: 'pk_live_Y2xlcmsuemFpYS1zdWl0ZXMuY29tJA',
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY_DEV: 'pk_test_cG9ldGljLXB1cC05My5jbGVyay5hY2NvdW50cy5kZXYk',
   },
   reactStrictMode: true,
   images: {
